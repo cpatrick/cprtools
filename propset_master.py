a9897a5d53f9a16fd12908e4b934c810906954e1
Patrick Reynolds <cpreynolds@gmail.com>
2009-08-18 16:55:58 -0400

    Added my recursive propsetting file to the repo.

---
+++ propset_master.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+text_extensions = (".txt",".cmake",".cxx",".txx",".h",".c",".py",".cpp",".sh",".xml",".cu",".bvh",".ui")
+
+
+curpath = ""
+if len(sys.argv) == 1:
+  curpath = "."
+else:
+  curpath = sys.argv[1]
+
+# Set basic things for text
+for root, dirs, files in os.walk( curpath ):
+  for file in [f for f in files if f.endswith(text_extensions)]:
+    os.system('svn propset svn:mime-type text/plain ' + os.path.join(root,file))
+    os.system('svn propset svn:eol-style native ' + os.path.join(root,file))
