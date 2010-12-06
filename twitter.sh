6fce5385237e96fea74a58bd80d777de8c8fa9b3
Patrick Reynolds <cpreynolds@gmail.com>
2010-12-06 11:24:48 -0500

    ENH: Removing old scripts that are no longer needed.

--- twitter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --basic --user "user:pass" --data-ascii "status=`echo $@|tr ' ' '+'`" "http://twitter.com/statuses/update.json"
--- weka.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export WEKA_HOME=/home/cpatrick/Dropbox/wekafiles
-CP=${CLASSPATH}:/home/cpatrick/Dropbox/wekafiles/weka.jar:/usr/share/java/mysql-connector-java.jar
-java -classpath ${CP} weka.gui.GUIChooser
