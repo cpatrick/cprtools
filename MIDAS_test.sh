b4f2e3f6fbed8c4499b154d039fff92a355a67a4
Patrick Reynolds <cpreynolds@gmail.com>
2010-12-13 12:54:31 -0500

    Adding some scripts for running midas tests to my toolkit.

---
+++ MIDAS_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/cpatrick/Source/MIDASserver-test
+/home/cpatrick/Source/MIDASserver-test/cake/console/cake -app /home/cpatrick/Source/MIDASserver-test/midas cdashtestsuite app all cdash
